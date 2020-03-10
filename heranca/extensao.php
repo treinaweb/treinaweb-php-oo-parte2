7ce58f9be06fdaaf5cb930c38512b291bd498ef7
Elton Fonseca <elton869@gmail.com>
2020-03-10 15:35:21 -0300

    3 - Características da classe especializada

--- heranca/extensao.php
+++ heranca/extensao.php
@@ -8,4 +8,6 @@ $obj->titulo = "Geladeira";
 $obj->preco = 900.00;
 $obj->defineCodigoBarras('00111');
 
+$obj->definirVoltagem(110);
+
 var_dump($obj);
--- src/Classes/Eletrodomestico.php
+++ src/Classes/Eletrodomestico.php
@@ -4,5 +4,12 @@ namespace App\Classes;
 
 class Eletrodomestico extends Produto
 {
-    
+    public int $voltagem;
+
+    public function definirVoltagem(int $voltagem): void
+    {
+        if ($voltagem === 110 || $voltagem === 220) {
+            $this->voltagem = $voltagem;
+        }
+    }
 }
