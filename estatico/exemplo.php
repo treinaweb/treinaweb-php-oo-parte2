1e1898c82b929497205d72c93dd6b5b5e722d637
Elton Fonseca <elton869@gmail.com>
2020-03-24 15:04:28 -0300

    34 - Propriedade estática

--- estatico/exemplo.php
+++ estatico/exemplo.php
@@ -2,4 +2,13 @@
 
 require_once "../autoload/autoload-psr4.php";
 
-echo App\Classes\Vendedor::comissao();
+use App\Classes\Vendedor;
+
+echo Vendedor::comissao();
+
+var_dump(Vendedor::$bonus);
+
+Vendedor::$bonus = 1.5;
+
+var_dump(Vendedor::$bonus);
+
--- src/Classes/Vendedor.php
+++ src/Classes/Vendedor.php
@@ -4,6 +4,8 @@ namespace App\Classes;
 
 class Vendedor
 {
+    static public float $bonus = 1.2;
+
     static public function comissao(): float
     {
         return 2.5;
